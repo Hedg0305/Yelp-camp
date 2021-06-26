2e26fa6fa15c104b7b4580c358753dd39c1fd9ef
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2021-06-26 15:50:58 -0300

    Login Authentication

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const path = require('path')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('connect-flash')
-
 const ExpressError = require('./utils/ExpressError')
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -44,19 +47,28 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash('success')
   res.locals.error = req.flash('error')
   next()
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -72,4 +84,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', {err})
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Running on 3000');
+})
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedsDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '60d7734521941f399404618f',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
